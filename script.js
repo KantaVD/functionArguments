e57345eda5dfb47e00b6778f7fed02ae9f037131
KantaVD <89583709+KantaVD@users.noreply.github.com>
2021-10-04 16:14:58 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+const wallPainting= function(wall, color) {
+    console.log("The "+ wall+ " has been painted " + color)
+};
+
+const colorGreen= ("green");
+const colorPink= ("pink")
+
+const wallN= ("north wall")
+const wallS= ("south Wall")
+
+wallPainting(wallN, colorGreen);
+wallPainting(wallS, colorPink);
+
+
